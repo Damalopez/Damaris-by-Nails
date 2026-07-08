a245e19a86667fd777165fbb7b7724d6dc7358a1
Damalopez <damarisbynails@gmail.com>
2026-07-07 20:15:18 -0500

    Implement scroll effect for navigation bar
    Added functionality to change navigation bar style on scroll.

--- script.js
+++ script.js
@@ -95,5 +95,16 @@ imagenes.forEach(img=>{
 fondo.addEventListener("click",()=>{
 
     fondo.classList.remove("activo");
-
+    
 });
+window.addEventListener("scroll", function () {
+    const nav = document.querySelector("nav");
+
+    if (window.scrollY > 50) {
+        nav.classList.add("scrolled");
+    } else {
+        nav.classList.remove("scrolled");
+    }
+});
+
+
